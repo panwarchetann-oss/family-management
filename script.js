1dee36b6c914b4397c319156380ebf5851dc6ed0
panwarchetann-oss <panwarchetann@gmail.com>
2025-12-30 16:27:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 // 🔴 YAHAN firebaseConfig paste hoga (next step)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyAtPPp9ImgOI8n4Zxi07aBConpZi4823bU",
+  authDomain: "family-management-bd626.firebaseapp.com",
+  projectId: "family-management-bd626",
+  storageBucket: "family-management-bd626.firebasestorage.app",
+  messagingSenderId: "783709611700",
+  appId: "1:783709611700:web:e3d1f267f6ab568b5d59e1",
+  measurementId: "G-HXQCLRQ9PZ"
 };
 
 const app = initializeApp(firebaseConfig);
