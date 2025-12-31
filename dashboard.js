e5ac26e1c292b1f6bd0c4abdd59da87ba31717d6
panwarchetann-oss <panwarchetann@gmail.com>
2025-12-31 16:06:32 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,41 +1,40 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { 
+  getFirestore, collection, addDoc 
+} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// 🔹 Firebase config (same as script.js)
-const firebaseConfig = {
-  apiKey: "AIzaSyAtPPp9ImgOI8n4Zxi07aBConpZi4823bU",
-  authDomain: "family-management-bd626.firebaseapp.com",
-  projectId: "family-management-bd626",
-  storageBucket: "family-management-bd626.firebasestorage.app",
-  messagingSenderId: "783709611700",
-  appId: "1:783709611700:web:e3d1f267f6ab568b5d59e1",
-  measurementId: "G-HXQCLRQ9PZ"
-};
+const db = getFirestore();
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+const form = document.getElementById("addMemberForm");
 
-onAuthStateChanged(auth, async (user) => {
-  if (!user) {
-    window.location.href = "index.html";
-    return;
-  }
+if (form) {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
-  const phone = user.phoneNumber;
-  const snap = await getDoc(doc(db, "users", phone));
+    const name = memberName.value.trim();
+    const phone = memberPhone.value.trim();
+    const dob = memberDOB.value.trim();
+    const blood = memberBlood.value.trim();
 
-  if (!snap.exists()) return;
+    if (!name || !phone) {
+      alert("Name & Phone required");
+      return;
+    }
 
-  const data = snap.data();
+    try {
+      await addDoc(collection(db, "familyMembers"), {
+        name: name,
+        phone: phone,
+        dob: dob,
+        bloodGroup: blood,
+        addedBy: localStorage.getItem("userPhone"),
+        createdAt: new Date()
+      });
 
-  document.getElementById("welcome").innerText =
-    `Welcome ${data.role.toUpperCase()}`;
+      alert("✅ Family Member Added Successfully");
+      form.reset();
+    } catch (err) {
+      alert(err.message);
+    }
+  });
+}
 
-  if (data.role === "admin") {
-    document.getElementById("adminPanel").style.display = "block";
-  } else {
-    document.getElementById("memberPanel").style.display = "block";
-  }
-});
